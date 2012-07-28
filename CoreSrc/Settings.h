bf6724ec0db5d30c1a263bf31ddf0d405c5b5d4e
bazhenovc <bazhenovc@gmail.com>
2012-07-28 19:29:36 +0300

    build fix

--- CoreSrc/Settings.h
+++ CoreSrc/Settings.h
@@ -15,12 +15,6 @@
 
 //------------------------------------------------------------------------------
 
-#if (defined _WIN32)
-#define NUTMEG_PLATFORM_WINDOWS
-#elif (defined __linux__)
-#define NUTMEG_PLATFORM_LINUX
-#endif
-
 //#define NUTMEG_COMPILER_BCPP
 #define NUTMEG_COMPILER_GPP
 #define NUTMEG_DEBUG
